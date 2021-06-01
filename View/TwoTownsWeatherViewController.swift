068cebb389b84d7e7f58cd83e68db9f2786ef5b4
Armelle <armelle.lecerf@orange.fr>
2021-06-01 22:36:29 +0200

    ajout de la deuxième page weather

---
+++ View/TwoTownsWeatherViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TwoTownsWeatherViewController.swift
+//  Balluchon2
+//
+//  Created by macmini-Armelle on 01/06/2021.
+//
+
+import UIKit
+
+class TwoTownsWeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
